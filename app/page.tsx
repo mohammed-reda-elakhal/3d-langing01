744cac9bf7fdc01a534fffb7d92cfbc7e1839af5
MOHAMMED REDA ELAKHAL <mohammedredaelakhale@gmail.com>
2024-01-14 21:20:07 +0100

    title change

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ function Navbar() {
   return (
     <div className='w-full h-16 backdrop-filter backdrop-blur-xl bg-opacity-20 border-b flex items-center justify-center'>
       <div className='max-w-7xl w-full flex items-center justify-between p-4'>
-        <h6 className='font-bold'>Insightful</h6>
+        <h6 className='font-bold'>Black Dev</h6>
         <ul className='flex gap-8'>
           <li><Link className='hover:text-fuchsia-500 transition-colors text-xs sm:text-base' href="#home">Home</Link></li>
           <li><Link className='hover:text-fuchsia-500 transition-colors text-xs sm:text-base' href="#about">About</Link></li>
