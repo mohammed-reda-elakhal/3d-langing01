a63351404a24da9f2d8c001db6c5cbd4a41752c3
MOHAMMED REDA ELAKHAL <mohammedredaelakhale@gmail.com>
2024-01-14 21:32:40 +0100

    chanege gif

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             </div>
         </div>
         <div className='w-full h-2/4 md:h-full md:w-3/5 flex items-center justify-center relative -z-10'>
-          <Spline className="w-full flex scale-[.25] sm:scale-[.35] lg:scale-[.5] items-center justify-center md:justify-start" scene='https://prod.spline.design/pvM5sSiYV2ivWraz/scene.splinecode'/>
+          <img className="w-full" src="/vd.gif" alt="Your GIF" />
         </div>
       </header>
       <section id="about" className="h-fit min-h-screen w-full flex relative items-center justify-center p-8">
